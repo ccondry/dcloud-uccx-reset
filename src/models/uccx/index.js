ac0662eaa81ade307e051f6fb0b7bc533777f461
Coty Condry <ccondry@cisco.com>
2019-11-14 12:31:39 -0600

    remove unused code

--- src/models/uccx/index.js
+++ src/models/uccx/index.js
@@ -1,49 +1,4 @@
 const uccx = require('./client')
-// 
-// async function deleteCsqs () {
-//   const skipped = []
-//   const success = []
-//   const fail = []
-//   console.log('listing CSQs...')
-//   const csqs = await uccx.csq.list()
-//   console.log('found', csqs.length, 'CSQs')
-//   const validTypes = ['Chat', 'Email', 'Voice', 'Outbound']
-//   const filtered = csqs.filter(csq => {
-//     // console.log(csq)
-//     // return csq.queueType === 'CHAT'
-//     // const prefix = csq.name.substring('
-//     const parts = csq.name.split('_')
-//     // valid csq has 2 parts
-//     if (parts.length !== 2) {
-//       skipped.push(csq.name)
-//       return false
-//     }
-//     // check prefix
-//     if (!validTypes.includes(parts[0])) {
-//       skipped.push(csq.name)
-//       return false
-//     }
-//     // check suffix - it should be a number
-//     if (isNaN(parts[1])) {
-//       skipped.push(csq.name)
-//       return false
-//     }
-//     //
-//     return true
-//   })
-//
-//   for (const csq of filtered) {
-//     // delete!
-//     try {
-//       await uccx.csq.delete(csq.id)
-//       console.log('successfully deleted UCCX CSQ', csq.name)
-//       success.push(csq.name)
-//     } catch (e) {
-//       console.log('failed to delete UCCX CSQ', csq.name, e.message)
-//       fail.push(csq.name)
-//     }
-//   }
-// }
 
 async function deleteCsqs () {
   return deleteItems({
